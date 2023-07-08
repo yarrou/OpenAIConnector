193f375363a0ff658a0dcd2726ebf683f7c761e0
yarrou <konic2884@gmail.com>
2023-07-08 07:06:28 +0200

    MemberData.java : this class is no longer needed, appropriate edits to the code of linked classes

--- app/src/main/java/de/alexkononsol/gptchatapp/entity/Message.java
+++ app/src/main/java/de/alexkononsol/gptchatapp/entity/Message.java
@@ -1,25 +1,41 @@
 package de.alexkononsol.gptchatapp.entity;
 
-public class Message {
-    private String text; // message body
-    private MemberData memberData; // data of the user that sent this message
-    private boolean belongsToCurrentUser; // is this message sent by us?
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 
-    public Message(String text, MemberData memberData, boolean belongsToCurrentUser) {
+public class Message {
+    private final String text; // message body
+    private final String userName;// name of the user that sent this message
+    private final String timeAndDate;
+    private final boolean belongsToCurrentUser; // is this message sent by us?
+
+
+    public Message(String text, String userName, boolean belongsToCurrentUser) {
         this.text = text;
-        this.memberData = memberData;
+        this.userName = userName;
         this.belongsToCurrentUser = belongsToCurrentUser;
+        this.timeAndDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+    }
+    public Message(String text, String userName, String timeAndDate , boolean belongsToCurrentUser) {
+        this.text = text;
+        this.userName = userName;
+        this.belongsToCurrentUser = belongsToCurrentUser;
+        this.timeAndDate = timeAndDate;
     }
 
     public String getText() {
         return text;
     }
 
-    public MemberData getMemberData() {
-        return memberData;
+    public String getUserName() {
+        return userName;
     }
 
     public boolean isBelongsToCurrentUser() {
         return belongsToCurrentUser;
+    }
+
+    public String getTimeAndDate() {
+        return timeAndDate;
     }
 }
--- app/src/main/java/de/alexkononsol/gptchatapp/utils/MessagesConverter.java
+++ app/src/main/java/de/alexkononsol/gptchatapp/utils/MessagesConverter.java
@@ -1,28 +1,18 @@
 package de.alexkononsol.gptchatapp.utils;
 
 import de.alexkononsol.gptchatapp.entity.ChatGPTMessage;
-import de.alexkononsol.gptchatapp.entity.MemberData;
 import de.alexkononsol.gptchatapp.entity.Message;
 
 public class MessagesConverter {
     public ChatGPTMessage convertToChatGPTMessage(Message message) {
-        ChatGPTMessage gptMessage = new ChatGPTMessage("gpt-3.5-turbo", message.getText());
-        return gptMessage;
-    }
-
-    public Message convertToMessage(String text, String name, String color) {
-        return new Message(text, new MemberData(name, color), false);
-    }
-
-    public Message convertToMessage(String text, MemberData data) {
-        return new Message(text, data, false);
+        return  new ChatGPTMessage("gpt-3.5-turbo", message.getText());
     }
 
     public Message convertToGPTMessage(String text) {
-        return new Message(text, new MemberData("gpt-3.5-turbo", "red"), false);
+        return new Message(text, "gpt-3,5 turbo", false);
     }
     public Message convertToMyMessage(String text) {
-        return new Message(text, new MemberData("I", "blue"), true);
+        return new Message(text, "I", true);
     }
 
 }
