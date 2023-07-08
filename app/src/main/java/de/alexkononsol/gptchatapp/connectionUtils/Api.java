4fd0d37f3dfc82cab9c87d1f80272b63fd57f6ba
yarrou <konic2884@gmail.com>
2023-07-08 06:55:32 +0200

    Api.java : Increased to 1 minute timeout for the request.

--- app/src/main/java/de/alexkononsol/gptchatapp/connectionUtils/Api.java
+++ app/src/main/java/de/alexkononsol/gptchatapp/connectionUtils/Api.java
@@ -1,20 +1,34 @@
 package de.alexkononsol.gptchatapp.connectionUtils;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class Api {
     public static ApiInterface getJsonClient(String url) {
-
+        OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                .connectTimeout(40, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .build();
         Retrofit retrofit = new Retrofit.Builder()
+                .client(okHttpClient)
                 .baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         return retrofit.create(ApiInterface.class);
     }
     public static ApiInterface getStringClient(String url){
+        OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                .connectTimeout(40, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .build();
         Retrofit retrofit = new Retrofit.Builder()
+                .client(okHttpClient)
                 .baseUrl(url)
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .build();
