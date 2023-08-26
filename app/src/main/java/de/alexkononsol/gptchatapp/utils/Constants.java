3a32e6e26513164d97a1f0a654deb00d524ee208
yarrou <konic2884@gmail.com>
2023-08-26 17:06:22 +0200

    server ip address changed

--- app/src/main/java/de/alexkononsol/gptchatapp/utils/Constants.java
+++ app/src/main/java/de/alexkononsol/gptchatapp/utils/Constants.java
@@ -3,7 +3,7 @@ package de.alexkononsol.gptchatapp.utils;
 public class Constants {
     //public static final String DEFAULT_HOST_URL = "http://192.168.178.198:8080";
     //public static final String DEFAULT_HOST_URL = "http://192.168.178.20:8080";
-    public static final String DEFAULT_HOST_URL = "http://138.68.122.155:8080";
+    public static final String DEFAULT_HOST_URL = "http://46.101.197.177:8080";
 
     public static final String SHARED_PREFERENCES_SETTINGS = "settings";
     public static final String SHARED_PREFERENCES_NAME = "name";
